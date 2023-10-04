286b36b731a63da2a96792396a98bdd053c95abc
Marius Florescu <50848249+MariusF-Dev@users.noreply.github.com>
2023-10-04 14:45:17 +0200

    added footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { AiOutlineGithub, AiOutlineLinkedin } from "react-icons/ai";
 
 const Footer = () => {
   return (
     <div className="mt-16 py-6 text-center bg-accent">
-      © Marius | All rights reserved 2023.
+      Built by Marius Florescu. Source code available on GitHub.
+      <br />
+      <a href='https://www.linkedin.com/in/mariusflorescu/' target="_blank" rel="noopener">
+        <AiOutlineLinkedin className="inline-block ml-2" size={25} /> 
+      </a> 
+      <a href='https://github.com/MariusF-Dev' target="_blank" rel="noopener">
+        <AiOutlineGithub className="inline-block ml-2" size={25} />
+      </a>
     </div>
   );
 };
